d64ed85176d15e738e615ff82f8cc2f5474836bc
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-04-27 10:34:37 +0100

    refacto CUstomCell

--- src/components/DataGrid/parts/CustomCell.tsx
+++ src/components/DataGrid/parts/CustomCell.tsx
@@ -2,40 +2,32 @@ import { formatValue } from '@ws-ui/webform-editor';
 import { MdCheck, MdClose } from 'react-icons/md';
 
 const CustomCell = ({ format, dataType, value }: { format: any; dataType: string; value: any }) => {
-  if (value && typeof value === 'object' && !(value instanceof Date)) {
-    return (
-      <>
-        {dataType === 'image' && value?.__deferred?.image ? (
-          <img className="image h-10 w-10" src={value?.__deferred?.uri} alt="" />
-        ) : (
-          JSON.stringify(value)
-        )}
-      </>
-    );
-  }
-
-  if (dataType === 'bool' && typeof value === 'boolean') {
-    // TODO: waiting fot formatting
-    if (format === 'checkbox') {
+  switch (true) {
+    case value && typeof value === 'object' && !(value instanceof Date):
+      return (
+        <>
+          {dataType === 'image' && value?.__deferred?.image ? (
+            <img className="image h-10 w-10" src={value?.__deferred?.uri} alt="" />
+          ) : (
+            JSON.stringify(value)
+          )}
+        </>
+      );
+    case dataType === 'number' && typeof value === 'boolean' && format === 'checkbox':
       return <input type="checkbox" checked={value} disabled />;
-    }
-    return value ? <MdCheck /> : <MdClose />;
-    //return value ? 'true' : 'false';
+    case dataType === 'number' && typeof value === 'boolean' && format === 'icon':
+      return value ? <MdCheck /> : <MdClose />;
+    case dataType === 'number' && typeof value === 'number' && format === 'slider':
+      return <input type="range" value={value} disabled />;
+    default:
+      const customValue =
+        value !== undefined && value !== null
+          ? format
+            ? formatValue(value, dataType, format)
+            : value.toString()
+          : value;
+      return <>{customValue}</>;
   }
-
-  if (dataType === 'number' && format === 'slider' && typeof value === 'number') {
-    // TODO: add more styling
-    return <input type="range" value={value} disabled />;
-  }
-
-  const Customvalue =
-    value !== undefined && value !== null
-      ? format
-        ? formatValue(value, dataType, format)
-        : value.toString()
-      : value;
-
-  return Customvalue;
 };
 
 export default CustomCell;
